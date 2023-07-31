9e05525e62829e84935395c2273d1231adbc069c
윤동현 <eatpaper1429@gmail.com>
2023-07-31 18:17:35 +0900

    동의어 적용 테스트

--- auto_restart.sh
+++ auto_restart.sh
@@ -3,16 +3,3 @@ docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
 docker-compose up
-
-while true; do
-    response=$(curl --write-out '%{http_code}' --silent --output /dev/null "http://localhost:9200")
-    if [ "$response" -eq "200" ]; then
-        echo "Elasticsearch is ready!"
-        break
-    else
-        echo "Waiting for Elasticsearch..."
-        sleep 5
-    fi
-done
-
-curl -X DELETE "localhost:9200/store"
