458b920a086540d01595ffa286f70eeb21799b4f
윤동현 <eatpaper1429@gmail.com>
2023-08-01 13:49:08 +0900

    동의어 적용 테스트

--- all_restart.sh
+++ all_restart.sh
@@ -1,3 +1,9 @@
 docker stop $(docker ps -a -q)
 
-docker-compose up
+docker rm $(docker ps -a -q)
+
+docker rmi $(docker images -q)
+
+docker volume rm $(docker volume ls)
+
+docker-compose up --build
--- auto_restart.sh
+++ auto_restart.sh
@@ -1,9 +1,3 @@
 docker stop $(docker ps -a -q)
 
-docker rm $(docker ps -a -q)
-
-docker rmi $(docker images -q)
-
-docker volume rm $(docker volume ls)
-
-docker-compose up --build
+docker-compose up
