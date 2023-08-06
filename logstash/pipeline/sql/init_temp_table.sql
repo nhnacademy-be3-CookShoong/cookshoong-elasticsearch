a402c6cee854f70e8f94db69b8c93383d00fa18c
윤동현 <eatpaper1429@gmail.com>
2023-08-06 22:26:18 +0900

    elastic 검색 정밀도 상승

--- logstash/pipeline/sql/init_temp_table.sql
+++ logstash/pipeline/sql/init_temp_table.sql
@@ -1,2 +1,3 @@
-SELECT stores.*, images.saved_name FROM cookshoong_shop_dev.stores
-join images on stores.store_image_id = images.image_id;
+SELECT stores.*, images.saved_name, addresses.latitude, addresses.longitude FROM cookshoong_shop_dev.stores
+join images on stores.store_image_id = images.image_id
+join addresses on stores.address_id = addresses.address_id;
