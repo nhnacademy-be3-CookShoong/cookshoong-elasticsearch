b4f8ab56b5c51dd839d083406cf09d7f2a0d05d2
윤동현 <eatpaper1429@gmail.com>
2023-08-07 09:54:19 +0900

    elastic 검색 정밀도 상승 - 메뉴 리스트 추가

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,13 +1,7 @@
 
 4
 .env,3/c/3c84dcdc6bbe3d7817c49dcdc327b926fea1808a
-H
-elasticsearch/Dockerfile,b/8/b83b36e0a5ff68b772194131023549a30126cc30
-A
-kibana/Dockerfile,b/6/b6392f12a8fd2a0ed040701e150829a1367b000c
 C
 logstash/Dockerfile,0/5/05fb195fd109de6075a36b57d29466590d9d09d7
-F
-mysql/config/mysql.cnf,8/6/86d9bd2fa250c5ce9633e31744828c9acda94937
 O
 logstash/pipeline/pipelines.yml,c/1/c156f422b974c192880a648c34429d1ba0cbd5ca
--- logstash/pipeline/sql/sync_temp_table.sql
+++ logstash/pipeline/sql/sync_temp_table.sql
@@ -1,3 +1,6 @@
-SELECT stores.*, images.saved_name, addresses.latitude, addresses.longitude FROM cookshoong_shop_dev.stores
-join images on stores.store_image_id = images.image_id
-join addresses on stores.address_id = addresses.address_id;
+SELECT stores.*, images.saved_name, addresses.latitude, addresses.longitude, GROUP_CONCAT(menu.name) as menus
+FROM cookshoong_shop_dev.stores
+         join images on stores.store_image_id = images.image_id
+         join addresses on stores.address_id = addresses.address_id
+         join menu on stores.store_id = menu.store_id
+GROUP BY stores.store_id;
