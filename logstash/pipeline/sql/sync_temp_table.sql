ce6bf516a39175893e5d4f9a67151a8068c8a7d2
윤동현 <eatpaper1429@gmail.com>
2023-08-07 10:13:30 +0900

    elastic 검색 정밀도 상승 - 메뉴 리스트 추가

--- logstash/pipeline/sql/sync_temp_table.sql
+++ logstash/pipeline/sql/sync_temp_table.sql
@@ -1,6 +1,8 @@
-SELECT stores.*, images.saved_name, addresses.latitude, addresses.longitude, GROUP_CONCAT(menu.name) as menus
+SELECT stores.*, images.saved_name, addresses.latitude, addresses.longitude, GROUP_CONCAT(menu.name) as menus, GROUP_CONCAT(DISTINCT store_categories.description) as categories
 FROM cookshoong_shop_dev.stores
          join images on stores.store_image_id = images.image_id
          join addresses on stores.address_id = addresses.address_id
          join menu on stores.store_id = menu.store_id
+         join stores_has_categories on stores.store_id = stores_has_categories.store_id
+         join store_categories on stores_has_categories.category_code = store_categories.category_code
 GROUP BY stores.store_id;
