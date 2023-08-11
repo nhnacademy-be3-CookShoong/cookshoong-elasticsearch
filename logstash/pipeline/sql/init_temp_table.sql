3a86649fab5b722a06216f0250411dcd7301e862
윤동현 <eatpaper1429@gmail.com>
2023-08-11 14:45:34 +0900

    prod 환경 elasticsearch dev 서버 version

--- logstash/pipeline/sql/init_temp_table.sql
+++ logstash/pipeline/sql/init_temp_table.sql
@@ -8,10 +8,10 @@ SELECT stores.*,
        GROUP_CONCAT(menu.name) as menus,
        GROUP_CONCAT(DISTINCT store_categories.description) as categories,
        MIN(menu.cooking_time) as minimum_cooking_time
-FROM cookshoong_shop_prod.stores
+FROM cookshoong_shop_dev.stores
          join images on stores.store_image_id = images.image_id
          join addresses on stores.address_id = addresses.address_id
-         join menu on stores.store_id = menu.store_id
-         join stores_has_categories on stores.store_id = stores_has_categories.store_id
-         join store_categories on stores_has_categories.category_code = store_categories.category_code
+         left join menu on stores.store_id = menu.store_id
+         left join stores_has_categories on stores.store_id = stores_has_categories.store_id
+         left join store_categories on stores_has_categories.category_code = store_categories.category_code
 GROUP BY stores.store_id;
