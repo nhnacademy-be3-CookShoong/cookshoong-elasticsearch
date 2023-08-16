d7d4b6c541173df69121b4ce7a346b395188fedf
윤동현 <eatpaper1429@gmail.com>
2023-08-16 14:56:09 +0900

    elasticsearch 리뷰 카운트 리뷰 평점 적용

--- auto_restart.sh
+++ auto_restart.sh
@@ -7,12 +7,12 @@ host=133.186.244.96" > /home/shoong/.my.cnf
 
 chmod 600 /home/shoong/.my.cnf
 
-mysql -Dcookshoong_shop_prod -B --skip-column-names -e"SELECT word_dict.word_text FROM cookshoong_shop_prod.word_dict;" > /home/shoong/cookshoong-elasticsearch/elasticsearch/config/analysis/userdict.txt
+mysql -Dcookshoong_shop_dev -B --skip-column-names -e"SELECT word_dict.word_text FROM cookshoong_shop_dev.word_dict;" > /home/shoong/cookshoong-elasticsearch/elasticsearch/config/analysis/userdict.txt
 
-mysql -Dcookshoong_shop_prod -B --skip-column-names -e"SELECT CONCAT(word_dict.word_text, ',', GROUP_CONCAT(wd.word_text ORDER BY wd.word_id)) as words
-                               FROM cookshoong_shop_prod.word_dict
-                               JOIN cookshoong_shop_prod.synonym_dict ON word_dict.word_id = synonym_dict.basic_word_id
-                               JOIN cookshoong_shop_prod.word_dict AS wd ON wd.word_id = synonym_dict.synonym_word_id
+mysql -Dcookshoong_shop_dev -B --skip-column-names -e"SELECT CONCAT(word_dict.word_text, ',', GROUP_CONCAT(wd.word_text ORDER BY wd.word_id)) as words
+                               FROM cookshoong_shop_dev.word_dict
+                               JOIN cookshoong_shop_dev.synonym_dict ON word_dict.word_id = synonym_dict.basic_word_id
+                               JOIN cookshoong_shop_dev.word_dict AS wd ON wd.word_id = synonym_dict.synonym_word_id
                                GROUP BY word_dict.word_id;" > /home/shoong/cookshoong-elasticsearch/elasticsearch/config/analysis/synonym.txt
 
 docker-compose up --build
--- logstash/pipeline/sql/sync_temp_table.sql
+++ logstash/pipeline/sql/sync_temp_table.sql
@@ -7,10 +7,14 @@ SELECT stores.*,
        addresses.main_place as place,
        GROUP_CONCAT(menu.name) as menus,
        GROUP_CONCAT(DISTINCT store_categories.description) as categories,
-       MIN(menu.cooking_time) as minimum_cooking_time
-FROM cookshoong_shop_prod.stores
+       MIN(menu.cooking_time) as minimum_cooking_time,
+       COUNT(DISTINCT review.review_id) as count_review,
+       AVG(review.rating) as count_rating
+FROM cookshoong_shop_dev.stores
          join images on stores.store_image_id = images.image_id
          join addresses on stores.address_id = addresses.address_id
+         left join orders on stores.store_id = orders.store_id
+         left join review on orders.order_code = review.order_code
          left join menu on stores.store_id = menu.store_id
          left join stores_has_categories on stores.store_id = stores_has_categories.store_id
          left join store_categories on stores_has_categories.category_code = store_categories.category_code
